b4e45ebf4f22a536ce4ed7475ed993ffafe131ff
Rossoket <rossomaxin.leonid@bk.ru>
2026-03-22 19:47:11 +0700

    Изменения с пары

--- Tables.h
+++ Tables.h
@@ -7,40 +7,42 @@
 #include <iostream>
 enum TokenType
 {
-   //dynamic
-   IDENTIFIER,
-   CONSTANT,
+    //dynamic
+    IDENTIFIER,
+    CONSTANT,
 
-   //static
-   KEYWORD,
-   OPERATOR,
-   SEPARATOR
-};
-
-struct functionValue
-{
-   ValueType value;
-   std::vector<ValueType> args;
-
+    //static
+    KEYWORD,
+    OPERATOR,
+    SEPARATOR
 };
 
 enum IdentificatorType
 {
-   VARIABLE,
-   FUNCTION
+    VARIABLE,
+    FUNCTION
 };
 enum ValueType
 {
-   NONE,
-   INT,
-   FLOAT,
-   CHAR,
+    NONE,
+    INT,
+    FLOAT,
+    CHAR,
+};
+
+struct functionValue
+{
+    ValueType value;
+    std::vector<ValueType> args;
+
 };
 
 struct LexemeAttributes
 {
-   void *value;
-   LexemeAttributes( void *value = nullptr) : value(value) {};
+    void* value;
+    
+    LexemeAttributes(void* value = nullptr) : value(value) {};
+
    ~LexemeAttributes()
    {
       if(value != nullptr)
@@ -52,91 +54,107 @@ struct LexemeAttributes
 
 struct Lexeme
 {
-   TokenType type;
-   size_t i;
-   Lexeme(TokenType type, size_t i) : type(type), i(i) {};
+    TokenType type;
+    size_t i;
+    Lexeme(TokenType type, size_t i) : type(type), i(i) {};
 };
 
 
 class StaticTable
 {
-   std::vector<std::string> lexems;
+    std::vector<std::string> lexems;
 public:
-   
-   StaticTable() = default;
-   void load(std::vector<std::string> &&_lexemes)
-   {
-      lexems = std::move(_lexemes);
-      std::sort(lexems.begin(), lexems.end());
-   }
-   size_t find(const std::string &lex)
-   {
-      size_t left = 0;
-      size_t right = lex.size() - 1;
-      while(left <= right)
-      {
-         size_t cur = (left + right) / 2;
-         if(lexems[cur] == lex)
-         {
-            return cur;
-         }
-      
-         if(lexems[cur] < lex)
-         {
-            left = cur + 1;
-         }
-         else
-         {
-            right = cur - 1;
-         }
-      }
-      return lexems.size();
-   }
-   size_t size()
-   {
-      return lexems.size();
-   }
-   std::string getVal(size_t i)
-   {
-      return lexems[i];
-   }
-   bool contains(const std::string &lex)
-   {
-      find(lex) == lexems.size() ? false : true;
-   }
+
+    StaticTable() = default;
+    void load(std::vector<std::string>&& _lexemes)
+    {
+        lexems = std::move(_lexemes);
+        std::sort(lexems.begin(), lexems.end());
+    }
+    size_t find(const std::string& lex)
+    {
+        size_t left = 0;
+        size_t right = lex.size() - 1;
+        while (left <= right)
+        {
+            size_t cur = (left + right) / 2;
+            if (lexems[cur] == lex)
+            {
+                return cur;
+            }
+
+            if (lexems[cur] < lex)
+            {
+                left = cur + 1;
+            }
+            else
+            {
+                right = cur - 1;
+            }
+        }
+        return lexems.size();
+    }
+    size_t size()
+    {
+        return lexems.size();
+    }
+    std::string getVal(size_t i)
+    {
+        return lexems[i];
+    }
+    bool contains(const std::string& lex)
+    {
+        find(lex) == lexems.size() ? false : true;
+    }
+    void drawTable()
+    {
+        for(size_t i = 0; i < lexems.size();i++)
+        {
+            std::cout << i << "  " << lexems[i] << std::endl;
+        }
+    
+    }
 };
 
 
 
 class DynamicTable
 {
-   std::unordered_map<std::string, size_t> identificators;
-   std::vector<std::string> IList;
-   std::vector<LexemeAttributes> attributes;
+    std::unordered_map<std::string, size_t> identificators;
+    std::vector<std::string> IList;
+    std::vector<LexemeAttributes*> attributes;
 public:
-   DynamicTable() = default;
-   size_t add(std::string &strlex, LexemeAttributes lexema)
-   {
-      size_t i = IList.size();
-      IList.push_back(strlex);
-      attributes.push_back(lexema);
-      identificators[strlex] = i;
-      return i;
-   }
-   auto find(const std::string &lex)
-   {
-      return (*identificators.find(lex)).second;
-   }
-   std::string getVal(size_t i)
-   {
-      return IList[i];
-   }
-   LexemeAttributes getAttributes(size_t i)
-   {
-      return attributes[i];
-   }
-   bool contains(const std::string &lex)
-   {
-      find(lex) == identificators.size() ? false : true;
-   }
+    DynamicTable() = default;
+    size_t add(std::string& strlex, LexemeAttributes *lexema)
+    {
+        if (identificators.find(strlex) == identificators.end())
+        {
+            size_t i = IList.size();
+            IList.push_back(strlex);
+            attributes.push_back(lexema);
+            identificators[strlex] = i;
+            return i;
+        }
+        return identificators.size();
+    }
+    auto find(const std::string& lex)
+    {
+        return identificators.find(lex);
+    }
+    std::string getVal(size_t i)
+    {
+        return IList[i];
+    }
+    int getAttributes( size_t i )
+    {
+       return *static_cast<int*>(attributes[i]->value);
+    }
+    bool contains(const std::string& lex)
+    {
+        return find(lex) == identificators.end() ? false : true;
+    }
+    size_t size()
+    {
+        return IList.size();
+    }
 };
--- main.cpp
+++ main.cpp
@@ -5,129 +5,198 @@
 int main()
 {
 
-   std::string testFolder = "test1/";
-   StaticTable keywords;
-   {
-      std::ifstream stream(testFolder + "Keywords.txt");
-      std::string buff = "";
-      std::vector<std::string> bArr;
-      while (std::getline(stream, buff))
-      {
-         bArr.push_back(buff);
-      }
-      keywords.load(std::move(bArr));
-   }
-   StaticTable operators;
-   {
-      std::ifstream stream(testFolder + "Operators.txt");
-      std::string buff = "";
-      std::vector<std::string> bArr;
-      while (std::getline(stream, buff))
-      {
-         bArr.push_back(buff);
-      }
-      operators.load(std::move(bArr));
-   }
-   StaticTable separators;
-   {
-      std::ifstream stream(testFolder + "Separators.txt");
-      std::string buff = "";
-      std::vector<std::string> bArr;
-      while (std::getline(stream, buff))
-      {
-         bArr.push_back(buff);
-      }
-      separators.load(std::move(bArr));
-   }
+    std::string testFolder = "test1/";
+    StaticTable keywords;
+    {
+        std::ifstream stream(testFolder + "Keywords.txt");
+        std::string buff = "";
+        std::vector<std::string> bArr;
+        while (std::getline(stream, buff))
+        {
+            bArr.push_back(buff);
+        }
+        keywords.load(std::move(bArr));
+    }
+    StaticTable operators;
+    {
+        std::ifstream stream(testFolder + "Operators.txt");
+        std::string buff = "";
+        std::vector<std::string> bArr;
+        while (std::getline(stream, buff))
+        {
+            bArr.push_back(buff);
+        }
+        operators.load(std::move(bArr));
+    }
+    StaticTable separators;
+    {
+        std::ifstream stream(testFolder + "Separators.txt");
+        std::string buff = "";
+        std::vector<std::string> bArr;
+        while (std::getline(stream, buff))
+        {
+            bArr.push_back(buff);
+        }
+        separators.load(std::move(bArr));
+    }
 
-   DynamicTable constants;
-   DynamicTable identificators;
-   std::string var = "x";
-   identificators.add(var, new LexemeAttributes());
+    DynamicTable constants;
+    DynamicTable identificators;
 
+    int command = 0;
+    std::string buf = "";
+    bool flag = true;
+    while (flag)
+    {
+        std::cout << "\nDynamic Table Terminal\n";
+        std::cout << "[1] Add variable\n";
+        std::cout << "[2] Add constant\n";
+        std::cout << "[3] Find index by name\n";
+        std::cout << "[4] Find var by index\n";
+        std::cout << "[5] Find const by index\n";
+        std::cout << "[6] Prove the element exitence\n";
+        std::cout << "[7] Write down a static table\n";
+        std::cout << "[0] Exit\n";
+        std::cout << "Command: \n";
+        std::cin >> command;
 
-
-
-
-   return 0;
-}
-
-
-void test1()
-{
-   std::string testFolder = "test1/";
-   StaticTable keywords;
-   {
-      std::ifstream stream(testFolder + "Keywords.txt");
-      std::string buff = "";
-      std::vector<std::string> bArr;
-      while (std::getline(stream, buff))
-      {
-         bArr.push_back(buff);
-      }
-      keywords.load(std::move(bArr));
-   }
-   StaticTable operators;
-   {
-      std::ifstream stream(testFolder + "Operators.txt");
-      std::string buff = "";
-      std::vector<std::string> bArr;
-      while (std::getline(stream, buff))
-      {
-         bArr.push_back(buff);
-      }
-      operators.load(std::move(bArr));
-   }
-   StaticTable separators;
-   {
-      std::ifstream stream(testFolder + "Separators.txt");
-      std::string buff = "";
-      std::vector<std::string> bArr;
-      while (std::getline(stream, buff))
-      {
-         bArr.push_back(buff);
-      }
-      separators.load(std::move(bArr));
-   }
-   DynamicTable constants;
-   DynamicTable identificators;
-   std::vector<Lexeme> lexems;
-   {
-      std::ifstream stream(testFolder + "Program.txt");
-      std::string buff = "";
-      int lspace = 0, rspace = 0;
-      while (std::getline(stream, buff))
-      {
-         rspace = buff.find(" ", lspace);
-         while (lspace != -1)
-         {
-            std::string substr = buff.substr(lspace, rspace == -1 ? buff.size() : rspace);
-
-            switch (0)
+        switch (command)
+        {
+        case 0:
+        {
+            flag = false;
+            break;
+        }
+        case 1:
+        {
+            std::cout << "Element name: ";
+            std::cin >> buf;
+            if (identificators.contains(buf))
             {
-               case 0:
-               {
-                  size_t i = keywords.find(substr);
-                  if (i != keywords.size())
-                  {
-                  }
-               }
-               case 1:
-               {
-
-
-               }
-
-
+                std::cout << "Such element already exists :(\n";
+                break;
             }
+            std::cout << "Value: ";
+            if (!(std::cin >> command))
+            {
+                std::cout << "It's not an int value! :o\n";
+                std::cin.ignore();
+                std::cin.clear();
+                break;
+            }
+            std::cout << "Element added at index " << identificators.add(buf, new LexemeAttributes(static_cast<void*>(new int(command)))) << std::endl;
+            break;
+        }
+        case 2:
+        {
+            std::cout << "Element name: ";
+            std::cin >> buf;
+            if (constants.contains(buf))
+            {
+                std::cout << "Such element already exists :(\n";
+                break;
+            }
+            std::cout << "Value: ";
+            if (!(std::cin >> command))
+            {
+                std::cout << "It's not an int value! :o\n";
+                std::cin.ignore();
+                std::cin.clear();
+                break;
+            }
+            std::cout << "Element added at index " << identificators.add(buf, new LexemeAttributes(static_cast<void*>(new int(command)))) << std::endl;
+            break;
+        }
+        case 3:
+        {
+            std::cout << "Element name: ";
+            std::cin >> buf;
+            if (identificators.contains(buf))
+            {
+                std::cout << "Found a variable: " << identificators.find(buf)->second << std::endl;
+                break;
+            }
+            if (constants.contains(buf))
+            {
+                std::cout << "Found a constant: " << constants.find(buf)->second << std::endl;
+                break;
+            }
+            std::cout << "No such element found :(\n";
+            break;
+        }
+        case 4:
+            std::cout << "Element index: ";
+            std::cin >> command;
+            if (command >= identificators.size())
+                std::cout << "No such element found :(\n";
+            else
+            {
+                std::cout << "Name: " << identificators.getVal(command) << std::endl;
+                std::cout << "Value: " << identificators.getAttributes(command) << std::endl;
+            }
+            break;
+        case 5:
+            std::cout << "Element index: ";
+            std::cin >> command;
+            if (command >= constants.size())
+                std::cout << "No such element found :(\n";
+            else
+            {
+                std::cout << "Name: " << constants.getVal(command) << std::endl;
+                std::cout << "Value: " << constants.getAttributes(command) << std::endl;
+            }
+            break;
+        case 6:
+        {
+            std::cout << "Element name: ";
+            std::cin >> buf;
 
-            lspace = rspace;
-            rspace = buff.find(" ", lspace);
-         }
-
-      }
-
-   }
-
-
+            if (identificators.contains(buf))
+                std::cout << "A variable exists! :)\n";
+            else if (constants.contains(buf))
+                std::cout << "A constant exists! :)\n";
+            else
+                std::cout << "Element doesn't exist! :(\n";
+            break;
+        }
+        case 7:
+        {
+            std::cout << "[1] Operators\n";
+            std::cout << "[2] Separators\n";
+            std::cout << "[3] Key words\n";
+            std::cout << "[0] Cancel\n";
+            if (!(std::cin >> command))
+            {
+                std::cout << "Don't try to break the program, pls :(\n";
+                std::cin.ignore();
+                std::cin.clear();
+                break;
+            }
+            std::cout << "\n";
+            switch (command)
+            {
+            case 0:
+                break;
+            case 1:
+                operators.drawTable();
+                break;
+            case 2:
+                separators.drawTable();
+                break;
+            case 3:
+                keywords.drawTable();
+                break;
+            default:
+                std::cout << "Unknown table :(\n";
+                break;
+            }
+            break;
+        }
+        default:
+        {
+            std::cout << "Unknown command. Bad luck :(\n";
+            break;
+        }
+        }
+    }
 }
